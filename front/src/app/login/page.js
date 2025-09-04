4d010d669574a7c34efee75762e8a5458bb7675b
tiziarias <tarias@pioix.edu.ar>
2025-09-04 18:32:13 -0300

    cambios en css

--- front/src/app/login/page.js
+++ front/src/app/login/page.js
@@ -1,60 +1,58 @@
-'use client'
+"use client";
 import Boton from "@/componentes/Boton";
 import Input from "@/componentes/Input";
 import { useEffect, useState } from "react";
-import styles from "@/app/login/page.module.css"
+import styles from "@/app/login/page.module.css";
 
 export default function Login() {
-    const [nombre, setNombre] = useState("")
-    const [contraseña, setContraseña] = useState("")
-    const [numero, setNumero] = useState(0)
-    const [nuevoUsuario, setNuevo] = useState(false)
+  const [nombre, setNombre] = useState("");
+  const [contraseña, setContraseña] = useState("");
+  const [numero, setNumero] = useState(0);
+  const [nuevoUsuario, setNuevo] = useState(false);
 
-    function modificarNombre(event) {
-        setNombre(event.target.value)
-    }
+  function modificarNombre(event) {
+    setNombre(event.target.value);
+  }
 
-    function modificarContraseña(event) {
-        setContraseña(event.target.value)
-    }
+  function modificarContraseña(event) {
+    setContraseña(event.target.value);
+  }
 
-    function modificarNumero(event) {
-        setNumero(event.target.value)
-    }
+  function modificarNumero(event) {
+    setNumero(event.target.value);
+  }
 
-    function checkboxActivado(event) {
-        console.log(event.target.checked)
-        setNuevo(event.target.checked)
-    }
+  function checkboxActivado(event) {
+    console.log(event.target.checked);
+    setNuevo(event.target.checked);
+  }
 
-    useEffect(() => {
-        console.log(nombre)
-    }, [nombre])
+  useEffect(() => {
+    console.log(nombre);
+  }, [nombre]);
 
-    useEffect(() => {
-        console.log(contraseña)
-    }, [contraseña])
-    
-    if (nuevoUsuario){
+  useEffect(() => {
+    console.log(contraseña);
+  }, [contraseña]);
 
-    } 
+  if (nuevoUsuario) {
+  }
 
-    return (
-        <div className={styles.todo}> 
-            <h1>Iniciar Sesión</h1>
-            <h2>nombre</h2>
-            <Input onChange={modificarNombre}></Input>
-            <h2>contraseña</h2>
-            <Input onChange={modificarContraseña}></Input>
-            <h2>número</h2>
-            <Input onChange={modificarNumero}></Input>
-            <h2> ¿eres nuevo?</h2>
-            <input type="checkbox" onChange={checkboxActivado}/>
+  return (
+    <div className={styles.todo}>
+      <div className={styles.container}>
+            <h1 className={styles.header}>Iniciar Sesión</h1>
+            <Input tipo="login" placeholder="Nombre" onChange={modificarNombre}></Input>
+            <Input tipo="login" placeholder="Contraseña" onChange={modificarContraseña}></Input>
+            <Input tipo="login" placeholder="Número" onChange={modificarNumero}></Input>
+            <h2 className={styles.h2}>¿Eres nuevo?</h2>
+            <input type="checkbox" onChange={checkboxActivado} />
             <h2>{nombre}</h2>
             <h2>{contraseña}</h2>
             <h2>{numero}</h2>
-            <Boton text="HOLA" tipo="login"></Boton>
-        </div>
-
-    )
+            <Boton text="INGRESAR" tipo="login"></Boton>
+      </div>
+    </div>
+    
+  );
 }
--- front/src/componentes/Input.js
+++ front/src/componentes/Input.js
@@ -1,9 +1,16 @@
 'use client'
+import styles from "@/componentes/Input.module.css"
+import clsx from "clsx"
 
 export default function Input(props){
     return(
 <>
-    <input type={props.type} onChange={props.onChange}/>
+    <input className={
+        clsx(
+            {[styles.estilosLogin]: props.tipo== "login"}
+        )
+
+    } type={props.type} onChange={props.onChange} placeholder={props.placeholder}/>
 </>
 )
 }
