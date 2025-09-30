6bc6451b16f6ca40de90cb6b848371270fd089da
simonAnesini <sanesini@pioix.edu.ar>
2025-09-30 09:01:58 -0300

    Delete front directory

--- front/fetch/index.js
+++
@@ -1,8 +0,0 @@
-async function traerUsuarios(){
-    let query = await fetch(`http://localhost:4000/traerUsuarios`, {
-        method: "POST",
-        headers:{"content-type" : "aplication/json"}
-        // body:JSON.stringify(dato)
-    })
-    let usuario
-}
